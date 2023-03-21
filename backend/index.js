ccdbe7c24d770b1aacfb1a4bcaefb3555f37a8be
Prajapati-Shivam <prajapatishivam11203@gmail.com>
2023-03-21 19:50:16 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,18 @@ const mongoose = require('mongoose');
 const mongoURI = 'mongodb://localhost:27017/cloud-book';
 const express = require('express');
 
+mongoose.set('strictQuery', false);
+mongoose.connect(mongoURI, () => {
+  console.log('Connected to MongoDB');
+})
+
+var cors = require('cors')
 
 const app = express();
 const port = 5000;
 
+app.use(cors())
+app.use(express.json())
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'));
@@ -13,9 +21,4 @@ app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
-})
-
-mongoose.set('strictQuery', false);
-mongoose.connect(mongoURI, () => {
-  console.log('Connected to MongoDB');
 })
