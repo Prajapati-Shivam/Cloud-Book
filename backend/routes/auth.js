1543c3249bd327f17acc89dbdbc1baa45db04762
Prajapati-Shivam <prajapatishivam11203@gmail.com>
2023-04-02 23:32:14 +0530

    add try catch block for create user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -14,29 +14,35 @@ router.post('/createuser', [
 ], async (req, res) => {
   let success = false;
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
-  }
-  let user = await User.findOne({ email: req.body.email });
-  if (user) {
-    return res.status(400).json({ success, error: "Sorry, this email is already registered" })
-  }
-  const salt = await bcrypt.genSalt(10);
-  const secPass = await bcrypt.hash(req.body.password, salt);
-  user = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: secPass,
-  })
-
-  const data = {
-    user: {
-      id: user.id
+  try {
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
     }
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ success, error: "Sorry, this email is already registered" })
+    }
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: secPass,
+    })
+  
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, process.env.REACT_APP_JWT_SECRET)
+    success = true;
+    res.json({ success, authtoken })
   }
-  const authtoken = jwt.sign(data, process.env.REACT_APP_JWT_SECRET)
-  success = true;
-  res.json({ success, authtoken })
+  catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
 })
 
 // ROUTE 2: Authenticate a user
