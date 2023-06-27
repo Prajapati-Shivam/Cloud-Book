b422b9ee4c796a12be6222b3125ddb2d92227003
Prajapati-Shivam <prajapatishivam11203@gmail.com>
2023-06-27 18:46:35 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.REACT_APP_MONGO_URI)
 const app = express();
 
 var cors = require('cors')
 
-const port = process.env.REACT_APP_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
